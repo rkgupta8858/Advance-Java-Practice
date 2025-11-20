6eff46124c3cabb4a7b808efea468222a02456c0
Rahul Kumar Gupta <rahulgupta9129@gmail.com>
2025-11-20 18:07:53 +0530

    first commit

---
+++ Spring MVC/Spring-WebMVC5/src/main/java/com/rahul/LoginController.java
@@ -0,0 +1,15 @@
+package com.rahul;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class LoginController {
+	@RequestMapping("/login")
+	public ModelAndView login() {
+		ModelAndView view = new ModelAndView();
+		view.setViewName("/loginPage");
+		return view;
+	}
+}
---
+++ Spring MVC/Spring-WebMVC5/src/main/webapp/index.jsp
@@ -0,0 +1,7 @@
+<html>
+<body>
+<h2>Hello Rahul!</h2>
+<h2>Welcome to Login Page.......</h2>
+<a href="login">Login</a>
+</body>
+</html>
